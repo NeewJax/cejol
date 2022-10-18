574ca55db06e2172b45f36e43de660643fd50e3c
NeewJax <103374260+NeewJax@users.noreply.github.com>
2022-10-18 18:59:12 -0300

    commit inicial
    primeiro commit

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilos/mobfirst.css">
+        <link rel="shortcut icon" href="imagens/CESJOL.png">
+        <title>Área do Aluno</title>
+    </head>
+    <body>
+        <header>
+                <h1>Área do Aluno</h1>
+        </header>
+        <img src="imagens/CESJOL.png" alt="">
+        <nav>
+            <ul>
+                <a href="#">
+                    <li>MATÉRIAS</li>
+                </a>
+                <a href="#">
+                    <li>HISTORICO</li>
+                </a>
+                <a href="#">
+                    <li>HORÁRIOS</li>
+                </a>
+                <a href="#">
+                    <li>DECLARAÇÃO</li>
+                </a>
+            </ul>
+        </nav>
+            <div class="margin">
+                <div id="cabec">
+                    <p class="mat">MATÉRIAS</p>
+                    <p class="hist">HISTÓRICO</p>
+                    <p class="hora">HORÁRIOS</p>
+                    <p class="decla">DECLARAÇÃO</p>
+                </div>
+                <div class="tabela">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><strong>Horários</strong></td>
+                                <td><strong>Segunda-feira</strong></td>
+                                <td><strong>Terça-feira</strong></td>
+                                <td><strong>Quarta-feira</strong></td>
+                                <td><strong>Quinta-feira</strong></td>
+                                <td><strong>Sexta-feira</strong></td>
+                            </tr>
+                            <tr>
+                                <td>07:30 - 08:20</td>
+                                <td>Disciplina <br>Nome do PROFESSOR</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>08:20 - 09:10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>09:10 - 10:00</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><strong>-</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                            </tr>
+                            <tr>
+                                <td>10:20 - 11:10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>11:10 - 12:00</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><strong>-</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                            </tr>
+                            <tr>
+                                <td>13:20 - 14:10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>14:10 - 15:00</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td><strong>-</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                                <td><strong>INTERVALO</strong></td>
+                            </tr>
+                            <tr>
+                                <td>15:20 - 16:10</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>16:10 - 17:00</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+    </body>
+</html>
